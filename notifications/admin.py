6fe1d46a51fe758214e7d39ba6f1b4fc48737b65
Arsen Ibatullin <arsen.ibatullin21@gmail.com>
2026-09-15 10:22:21 +0500

    Notifications: model/admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from notifications.models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'type', 'title']
--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class Notification(models.Model):
+    class NotificationType(models.TextChoices):
+        ORDER_CREATED = 'order_created', "Order_Created"
+        ORDER_PAID = 'order_paid', "Order_Paid"
+        ORDER_CANCELED = 'order_canceled', "Order_Canceled"
+        STOCK = 'stock', 'Stock'
+        SYSTEM = 'system', 'System'
+
+    user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE, related_name='notifications')
+    title = models.CharField(max_length=120)
+    type = models.CharField(choices=NotificationType.choices)
+    message = models.TextField(max_length=500)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        db_table = 'notifications'
+        verbose_name = 'Notification'
+        verbose_name_plural = 'Notifications'
+
+    def __str__(self):
+        return f"{self.user} -> {self.type}"
+
